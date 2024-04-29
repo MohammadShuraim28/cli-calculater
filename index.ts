119a0818213705f7bad0eebe3b0b7104bf7a7ef1
mohammadshuraim28 <mohammadshuraim192>
2024-04-29 16:27:16 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstnumber" },
+  { message: "enter second number", type: "number", name: "secondnumber" },
+  {
+    message: "select one of the operator to perform the action",
+    type: "list",
+    name: "operator",
+    choices: ["addition" , "substraction" , "multiplication" , "division"],
+  },
+]);
+
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+} else if (answer.operator === "substraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+} else {
+    console.log("please select valid operator");
+}
+
